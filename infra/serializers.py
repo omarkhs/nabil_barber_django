f7c5c3538e143418ccc54f06c3033cfa75484287
Ahmed Elmaghraby <a.elmaghraby@alumni.ubc.ca>
2017-12-03 15:27:04 -0800

    more tests added

--- infra/serializers.py
+++ infra/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from infra.models import Profile
 from django.contrib.auth.models import User
+from django.core import exceptions
+import django.contrib.auth.password_validation as validators
 
 
 # Extra fields added to User class
@@ -15,7 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email',
-                'profile',)
+                'profile', 'password',)
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
@@ -25,9 +27,48 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ('username', 'password', 'first_name', 'last_name', 'email',
                 'profile',)
         extra_kwargs = {'email': {'required': 'True'}}
+        write_only_fields = ('password',)
+
+    def to_representation(self, obj):
+        ret = super(RegistrationSerializer, self).to_representation(obj)
+        ret.pop('password')
+        return ret
+
+    # def validate_password(self, value):
+        # try:
+            # validators.validate_password(value)
+        # except serializers.ValidationError as exc:
+            # raise serializers.ValidationError(str(exc))
+        # return value
+    def validate(self, data):
+        # validators.validate_password(password=data, user=User)
+        # return data
+
+        # here data has all the fields which have validated values
+        # so we can create a User instance out of it
+        data.pop('profile')
+        user = User(**data)
+
+        # get the password from the data
+        password = data.get('password')
+
+        errors = dict()
+        try:
+            # validate the password and catch the exception
+            validators.validate_password(password=password, user=user)
+
+        # the exception raised here is different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super(RegisterUserSerializer, self).validate(data)
 
     def create(self, validated_data):
         profile_data = validated_data.pop('profile')
         user = User.objects.create(**validated_data)
+        user.set_password( validated_data.pop('password') )
         Profile.objects.create(user=user, **profile_data)
         return user
--- infra/tests.py
+++ infra/tests.py
@@ -6,15 +6,46 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 
+import json
+
 # Create your tests here.
-class UserTestCase(APITestCase):
+class UserRegistrationTestCase(APITestCase):
 
-    def test_get_all_users( self ):
-        response = self.client.post('/user')
-        data = {'username': 'ahmed', 'password':'somepass', 'email':'ahmed@test.com',
-                'profile':{ 'phone_number' : '+16043621800'} }
+    def setUp( self ):
+        self.userInfo = {'username': 'jackie', 'first_name':'Jack',
+        'last_name':'Bell', 'email':'test@test.com',
+                'profile':{ 'phone_number' : '+16473459800'} }
+
+    def test_register_short_password( self ):
+        '''
+        Password must be less than 8 characters for the test to fail
+        '''
+        data = self.userInfo
+        self.userInfo[ 'password' ] = 'short'
         response = self.client.post('/user/', data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        response = self.client.get('/user/')
-        print response.content
+        self.assertEqual( response.status_code, status.HTTP_400_BAD_REQUEST )
 
+    def test_register_similar_password( self ):
+        '''
+        Password must be similar to username or any other attribute for the test to fail
+        '''
+        data = self.userInfo
+        self.userInfo[ 'password' ] = 'jackie2'
+        response = self.client.post('/user/', data, format='json')
+        self.assertEqual( response.status_code, status.HTTP_400_BAD_REQUEST )
+
+    def test_register_password_all_numbers( self ):
+        data = self.userInfo
+        self.userInfo[ 'password' ] = '4549841514684'
+        response = self.client.post('/user/', data, format='json')
+        self.assertEqual( response.status_code, status.HTTP_400_BAD_REQUEST )
+
+    def test_register_password_is_common( self ):
+        '''
+        Common password are based on this list:
+            https://docs.djangoproject.com/en/1.11/topics/auth/passwords/#included-validators
+        '''
+        data = self.userInfo
+        self.userInfo[ 'password' ] = 'trustno1'
+        response = self.client.post('/user/', data, format='json')
+        self.assertEqual( response.status_code, status.HTTP_400_BAD_REQUEST )
